c3f99ec210506fd458427e5e4dbc3d81e62b2abb
Nicholas Magulick <nmagulick@mitre.org>
2014-06-05 12:08:49 -0400

    Initial version

--- keygen.cpp
+++ keygen.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
+void generator(vector<char> &c);
+
 int main()
 {
-	
+	srand(time(NULL));
+
+	vector<char> key;
+	ofstream  fileOut ("key.txt");
+
+	generator(key);
+
+	for(int i = 0; i < key.size(); i++)
+	{
+		fileOut << key[i];
+	}
+
+	fileOut << "\n";
+
+	fileOut.close();
+}
+
+void generator(vector<char> &c)
+{
+	int count = 0;
+	string alpha = "1234567890abcdefghijklmnopqrstuvwxyz";
+
+	while(count < 40)
+	{
+		c.push_back(alpha[rand() % alpha.size()]);
+
+		count++;
+	}
 }
