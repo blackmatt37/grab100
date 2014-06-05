076c6027aa051a000bee425e9d2cdb909a6bdbff
Nicholas Magulick <nmagulick@mitre.org>
2014-06-05 12:41:45 -0400

    Added file I/O to write key to file

--- keygen.cpp
+++ keygen.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <ctime>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -10,16 +11,20 @@ int main()
 	srand(time(NULL));
 	vector<char> key;
 	string alpha = "1234567890abcdefghijklmnopqrstuvwxyz";
+	ofstream  fileOut ("key.txt");
 
 	for(int i = 0; i < 40; i++)
 	{
 		key.push_back(alpha[rand() % 36]);
 	}
 
+
 	for(int j = 0; j < key.size(); j++)
 	{
-		cout << key[j];
+		fileOut << key[j];
 	}
 
-	cout << "\n";
+	fileOut << "\n";
+
+
 }
