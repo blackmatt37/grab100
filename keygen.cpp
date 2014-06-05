8a2552749275b1b3dc5fed898322484878893fb7
Nicholas Magulick <nmagulick@mitre.org>
2014-06-05 12:08:49 -0400

    Added variables and first version of generator

--- keygen.cpp
+++ keygen.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	
+	srand(time(NULL));
+	vector<char> key;
+	string alpha = "1234567890abcdefghijklmnopqrstuvwxyz";
+
+	for(int i = 0; i < 40; i++)
+	{
+		key.push_back(alpha[rand() % 36]);
+	}
 }
